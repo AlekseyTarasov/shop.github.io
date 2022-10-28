d667f3bd8604ec93fdf8963bd4a6f2d1cfe0ae7e
Aleksey Tarasov <chuprinalexis@gmail.com>
2022-10-28 16:31:18 +0300

    up

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,5 +1,5 @@
 simpleCart.ready(function() {
   simpleCart.each(function(item, x) {
-    $('form').append('<input type="hidden" name=" Name" value="' + item.get('name') + '">');
+    $('form').append('<input type="hidden" name="Name" value="' + item.get('name') + '"/>');
   });
 });
