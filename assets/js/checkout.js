c45f0beb5e27446a0f41f3c0c9ca05dbdd25bcbc
Aleksey Tarasov <chuprinalexis@gmail.com>
2022-10-28 17:00:31 +0300

    Update checkout.js

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,9 +1,10 @@
 simpleCart.ready(function() {
   simpleCart.each(function(item, x) {
     $('form').append('<input type="hidden" name="Name" value="' + item.get('name') + '"/>');
-    $('form').append('<input type="hidden" name="Price" value="$' + item.get('price') + '"/>');
+    $('form').append('<input type="hidden" name="Price" value="' + item.get('price') + '"/>');
     $('form').append('<input type="hidden" name="Size" value="' + item.get('size') + '"/>');
     $('form').append('<input type="hidden" name="Color" value="' + item.get('color') + '"/>');
-
+    $('form').append('<input type="hidden" name="Quantity" value="' + item.get('quantity') + '"/>');
+    $('form').append('<input type="hidden" name="Total price" value="' + item.get('total') + '"/>');
   });
 });
